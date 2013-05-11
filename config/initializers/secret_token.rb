8da5d4e5cea9d00a46b70a837b2999f4464f8d6b
Junichi Ito <jit@sonicgarden.jp>
2013-05-11 13:04:11 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CbPal::Application.config.secret_token = 'c0c5b9045b2d6adcce6ccbfe68111df5737927c4bc0cd9cd830ac3b77dfeccccfbe568f36520e55d26c7b39cf83004f7358a6f5bcc7aa72f259dbfc8dc8e55e6'
