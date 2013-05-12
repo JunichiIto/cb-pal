f2e8d46f5d6c2534a239d6891fa2637366ae6496
Junichi Ito <jit@sonicgarden.jp>
2013-05-12 12:17:04 +0900

    Implement create with details

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ActiveRecord::Base
-  has_many :orders
-  attr_accessible :customer_name
+  has_many :order_details
+  attr_accessor :bread_quantities
+  attr_accessible :customer_name, :bread_quantities
+
+  before_save :create_details
+  def create_details
+    return if bread_quantities.empty?
+    bread_quantities.select{|_, qty| qty.to_f > 0.0}.each do |bread_id, quantity|
+      bread = Bread.find bread_id
+      detail = self.order_details.build
+      detail.bread = bread
+      detail.quantity = quantity.to_f
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,52 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe Order do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "::create" do
+    before do
+      menu = Menu.new
+      menu.quantity = 5
+      @bread1 = menu.build_bread
+      @bread1.name = "クリームぱん"
+      menu.save!
+
+      menu = Menu.new
+      menu.quantity = 5
+      @bread2 = menu.build_bread
+      @bread2.name = "あんぱん"
+      menu.save!
+
+      menu = Menu.new
+      menu.quantity = 5
+      @bread3 = menu.build_bread
+      @bread3.name = "バゲット"
+      menu.save!
+
+      @param = {"customer_name"=>"たなか", "bread_quantities"=>{@bread1.id.to_s => "1", @bread2.id.to_s => "0", @bread3.id.to_s => "0.5"}}
+    end
+
+    subject { Order.create @param }
+
+    it 'creates order' do
+      expect(subject).to be_persisted
+    end
+
+    it 'saves customer_name' do
+      expect(subject.customer_name).to eq "たなか"
+    end
+
+    it 'creates order details' do
+      expect(subject.order_details).to have(2).items
+    end
+
+    it 'creates order detail for クリームぱん' do
+      detail = subject.order_details.where(bread_id: @bread1.id).first
+      expect(detail.quantity).to eq 1.0
+    end
+
+    it 'creates order detail for バゲット' do
+      detail = subject.order_details.where(bread_id: @bread3.id).first
+      expect(detail.quantity).to eq 0.5
+    end
+  end
 end
